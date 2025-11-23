cff8bdde256655c00c9939e430023916258321e8
abdullah-altedlawi <abode02651@gmail.com>
2025-11-23 21:23:42 +0300

    Merge branch 'Abdullah-T'

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -7,8 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform cameraTransform;
 
-private Vector2 lookInput;
-
     [Header("Animation")]
     private Animator animator;
 
